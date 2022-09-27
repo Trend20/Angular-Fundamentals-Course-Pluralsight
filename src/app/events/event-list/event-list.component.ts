e0f2c96b018d6266e51251e0a626ca875c6a7c91
Trend20 <enockomondi305@gmail.com>
2022-09-27 15:17:23 +0300

    event list content

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
+  event={
+    id: 1,
+    name: 'Angular Connect',
+    date: '12/03/2022',
+    time: '10:30am',
+    price: 499.90,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location:{
+      address: '00100 NRB',
+      city: 'Nairobi',
+      country: 'Kenya'
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
