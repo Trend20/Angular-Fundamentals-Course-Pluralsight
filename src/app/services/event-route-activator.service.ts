dd563d87bd82d830cca3fa87f5cb15bb10638182
Trend20 <enockomondi305@gmail.com>
2022-09-29 16:15:11 +0300

    adding route guard

--- src/app/services/event-route-activator.service.ts
+++ src/app/services/event-route-activator.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router} from '@angular/router';
+import { EventsService } from '../events/shared/events.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventRouteActivatorService {
+export class EventRouteActivatorService implements CanActivate{
 
-  constructor() { }
+  constructor(private router: Router, private eventService: EventsService) { }
+
+  canActivate(route: ActivatedRouteSnapshot){
+     const eventExist =  !!this.eventService.getEvent(+route.params['id'])
+
+     if(!eventExist){
+      this.router.navigate(['/404'])
+     }
+
+     return eventExist;
+  }
 }
