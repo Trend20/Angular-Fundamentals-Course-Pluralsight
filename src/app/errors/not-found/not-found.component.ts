db73425b5cb1d109d61279886ca7d3f5966b37ee
Trend20 <enockomondi305@gmail.com>
2022-09-29 14:16:15 +0300

    not found page component

--- src/app/errors/not-found/not-found.component.ts
+++ src/app/errors/not-found/not-found.component.ts
@@ -2,8 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  template: `
+    <h1 class="errorMessage">404'd</h1>
+  `,
+  styles: [`
+    .errorMessage {
+      margin-top:150px;
+      font-size: 170px;
+      text-align: center;
+    }`]
 })
 export class NotFoundComponent implements OnInit {
 
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { EventListComponent } from "./events/event-list/event-list.component";
 import { NewEventComponent } from "./events/new-event/new-event.component";
@@ -15,6 +16,10 @@ export const appRoutes: Routes =[
   {
     path: 'events/:id',
     component: EventDetailsComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
   },
   {
     path: '',
