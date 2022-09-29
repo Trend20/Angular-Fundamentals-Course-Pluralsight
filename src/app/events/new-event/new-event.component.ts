859637ccc96d898b41a5dc5a27c381ffcd98ba1c
Trend20 <enockomondi305@gmail.com>
2022-09-29 14:09:59 +0300

    new event component

--- src/app/events/new-event/new-event.component.ts
+++ src/app/events/new-event/new-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-event',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEventComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  // navigate back to the events page
+  cancelEvent(){
+    this.router.navigate(['/events']);
+  }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { EventListComponent } from "./events/event-list/event-list.component";
+import { NewEventComponent } from "./events/new-event/new-event.component";
 
 export const appRoutes: Routes =[
+  {
+    path: 'events/new',
+    component: NewEventComponent
+  },
   {
     path: 'events',
     component: EventListComponent
