fa150afbd6e7d0fabb46cb9803cd0328a933d770
Trend20 <enockomondi305@gmail.com>
2022-10-03 15:41:45 +0300

    adding resolver service

---
+++ src/app/services/event-list-resolver.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { EventsService } from '../events/shared/events.service';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventListResolverService implements Resolve<any>{
+
+  constructor(private eventService: EventsService) { }
+
+  resolve(){
+    return this.eventService.getEvents().pipe(map(events => events))
+  }
+}
